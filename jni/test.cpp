bf78cf7e9ea58e3ef2887fee3b5f6633f33ff354
amyMeng <393952590@qq.com>
2017-08-26 17:42:40 +0800

    init

---
+++ jni/test.cpp
@@ -0,0 +1,91 @@
+#include <jni.h>
+#include "substrate.h"
+#include <android/log.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define TAG "HOOKTEST"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+
+//MSConfig(MSFilterLibrary, "libdvm.so");
+MSConfig(MSFilterLibrary, "libc.so");
+
+
+bool (*_dvmLoadNativeCode)(char* pathName, void* classLoader, char** detail);
+
+bool fake_dvmLoadNativeCode(char* soPath, void* classLoader, char** detail)
+{
+	LOGD("fake_dvmLoadNativeCode soPath:%s", soPath);
+	return _dvmLoadNativeCode(soPath,classLoader,detail);
+}
+
+////Substrate entry point
+//MSInitialize{
+//    LOGD("Substrate initialized.");
+//    MSImageRef image;
+//    image = MSGetImageByName("/system/lib/libdvm.so"); // 绝对路径
+//    if (image != NULL)
+//    {
+//    	LOGD("dvm image: 0x%08X", (void*)image);
+//
+//        void * dvmload = MSFindSymbol(image, "_Z17dvmLoadNativeCodePKcP6ObjectPPc");
+//        if(dvmload == NULL)
+//        {
+//            LOGD("error find dvmLoadNativeCode.");
+//        }
+//        else
+//        {
+//        	MSHookFunction(dvmload,(void*)&fake_dvmLoadNativeCode,(void **)&_dvmLoadNativeCode);
+//			LOGD("hook dvmLoadNativeCode sucess.");
+//        }
+//    }
+//    else{
+//        LOGD("can not find libdvm.");
+//    }
+//}
+
+
+int (*old__system_property_get)(const char *name, char *value);
+int new__system_property_get(const char *name, char *value)
+{
+	LOGD("new__system_property_read  name:%s, value:%s",name,value);
+	if(strstr(name,"ro.serialno")) return strlen(strcpy(value,"AABBCC"));
+	else if(strstr(name,"ro.product.model")) return strlen(strcpy(value,"2014814"));
+	else return old__system_property_get(name,value);
+
+//    int result=old__system_property_get(name,value);
+//    return result;
+}
+//程序入口
+MSInitialize
+{
+    MSImageRef image = MSGetImageByName("/system/lib/libc.so");
+    if (image != NULL)
+    {
+        void *hook__system_property_read=MSFindSymbol(image,"__system_property_get");
+        if(hook__system_property_read) MSHookFunction(hook__system_property_read,(void*)&new__system_property_get,(void **)&old__system_property_get);
+        else LOGD("hook dvmLoadNativeCode NULL.");//LOGE("error find __system_property_read ");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ obj/local/armeabi/objs/test.cy/test.o.d
@@ -0,0 +1,164 @@
+F:/Work/GIT/cydia-android-hook//obj/local/armeabi/objs/test.cy/test.o: \
+ F:/Work/GIT/cydia-android-hook//jni/test.cpp \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/jni.h \
+ F:/Work/GIT/cydia-android-hook//jni/substrate.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/dlfcn.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/string.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/malloc.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/alloca.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/strings.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/stdint.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/memory.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/android/log.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/unistd.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/signal.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/limits.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/stdio.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/endian.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/endian.h
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/jni.h:
+
+F:/Work/GIT/cydia-android-hook//jni/substrate.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/dlfcn.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/string.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/strings.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/memory.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/signal.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/limits.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/endian.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+D:/ProgramFilesStudioSdk/NDK/platforms/android-3/arch-arm/usr/include/machine/endian.h:
